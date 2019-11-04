53155d9651c77a230188954bf7d7b16b786652fc
Gustavo Perez <ggusperezg@gmail.com>
2019-11-04 17:36:23 -0600

    Correccion-Tamaño-VentanaCI

--- DLL/Navegador/CapaDeDiseno/Compleja.Designer.cs
+++ DLL/Navegador/CapaDeDiseno/Compleja.Designer.cs
@@ -34,7 +34,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1008, 641);
+			this.ClientSize = new System.Drawing.Size(1016, 663);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.Name = "Compleja";
 			this.Text = "Compleja";
